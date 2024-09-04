59950279350733fbc994ff7854fcdee0a5d81211
Christopher Henry <chenry@anl.gov>
2024-09-03 23:52:18 -0500

    Fixing directory name

--- google_collab.py
+++ google_collab.py
@@ -16,14 +16,14 @@ class MSColabUtils:
       if exists('/Environment') and overwrite:
         shutil.rmtree('/Environment')
       if not exists('/Environment'):
-        with tarfile.open("/content/drive/MyDrive/MyMSCollab/MSGoogleColab/Env.tgz", 'r:gz') as tar:
+        with tarfile.open("/content/drive/MyDrive/MyMSColab/MSGoogleColab/Env.tgz", 'r:gz') as tar:
           tar.extractall(path="/")
 
     def SetupData(self,overwrite=False):
       if exists('/Data') and overwrite:
         shutil.rmtree('/Data')
       if not exists('/Data'):
-        with tarfile.open("/content/drive/MyDrive/MyMSCollab/MSGoogleColab/data.tgz", 'r:gz') as tar:
+        with tarfile.open("/content/drive/MyDrive/MyMSColab/MSGoogleColab/data.tgz", 'r:gz') as tar:
           tar.extractall(path="/")
     
     def SetToken(self,token):
@@ -33,20 +33,20 @@ class MSColabUtils:
         file.write(token)
 
     def SetupColabHome(self,overwrite=False):
-      if exists('/content/drive/MyDrive/MyMSCollab') and overwrite:
-        shutil.rmtree('/content/drive/MyDrive/MyMSCollab')
-      if not exists('/content/drive/MyDrive/MyMSCollab'):
-        os.makedirs('/content/drive/MyDrive/MyMSCollab')
-      if not exists('/content/drive/MyDrive/MyMSCollab/MSGoogleColab'):
-        result = subprocess.run("cd /content/drive/MyDrive/MyMSCollab;git clone https://github.com/ModelSEED/MSGoogleColab.git", stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
-      if not exists('/content/drive/MyDrive/MyMSCollab/config'):
-        shutil.copyfile('/content/drive/MyDrive/MyMSCollab/MSGoogleColab/defaultconfig','/content/drive/MyDrive/MyMSCollab/config')
+      if exists('/content/drive/MyDrive/MyMSColab') and overwrite:
+        shutil.rmtree('/content/drive/MyDrive/MyMSColab')
+      if not exists('/content/drive/MyDrive/MyMSColab'):
+        os.makedirs('/content/drive/MyDrive/MyMSColab')
+      if not exists('/content/drive/MyDrive/MyMSColab/MSGoogleColab'):
+        result = subprocess.run("cd /content/drive/MyDrive/MyMSColab;git clone https://github.com/ModelSEED/MSGoogleColab.git", stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
+      if not exists('/content/drive/MyDrive/MyMSColab/config'):
+        shutil.copyfile('/content/drive/MyDrive/MyMSColab/MSGoogleColab/defaultconfig','/content/drive/MyDrive/MyMSColab/config')
     
     def LinkColabHome(self,overwrite=False):
       if exists("/root/.kbase") and overwrite:
         shutil.rmtree("/root/.kbase")
       if not exists("/root/.kbase"):
-        os.symlink("/content/drive/MyDrive/MyMSCollab","/root/.kbase")
+        os.symlink("/content/drive/MyDrive/MyMSColab","/root/.kbase")
 
     def LinkNotebookDirectory(self,notebook_dir,name):
       if not exists("/content/"+name):
@@ -54,7 +54,7 @@ class MSColabUtils:
 
     def UpdateSourceEnvironment(self):
       if exists('/Environment'):
-         with tarfile.open("/content/drive/MyDrive/MyMSCollab/MSGoogleColab/Env.tgz", "w:gz") as tar:
+         with tarfile.open("/content/drive/MyDrive/MyMSColab/MSGoogleColab/Env.tgz", "w:gz") as tar:
             tar.add('/Environment', arcname=os.path.basename('/Environment'))
 
 gc_util = MSColabUtils()
